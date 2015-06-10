5b7781c0e47309186be5ea9c38bc94928e89b4e8
Jean-Baptiste Note <jb.note@criteo.com>
2015-06-10 14:08:27 +0200

    Starting to get the gist of it

--- druid-slider-package/package/scripts/druid.py
+++ druid-slider-package/package/scripts/druid.py
@@ -19,21 +19,34 @@ class Druid(Script):
     import params
     env.set_params(params)
 
+  def hostname(self):
+    import socket
+    return socket.getfqdn()
+
   def start(self, env):
     import params
     env.set_params(params)
     self.configure(env)
 
     # Dump the configuration dependent on the dictionary name
-    node_type=name()
-    PropertiesFile(format("{params.conf_dir}/{node_type}/runtime.properties"),
-                   properties = params.configs[node_type],
+    PropertiesFile(format("{params.config_dir}/_common/common.runtime.properties"),
+                   properties = params.configs['_common'],
+                   owner=params.app_user)
+
+    node_type=self.name()
+    node_config=params.configs[node_type]
+    hostname=self.hostname()
+    node_config.update({'druid.host':format("{hostname}:{node_config[druid.port]}")})
+    PropertiesFile(format("{params.config_dir}/{node_type}/runtime.properties"),
+                   properties = node_config,
                    owner=params.app_user)
 
     # Merge shared configuration with local one (or not ?)
 
     # XXX port is where ?
-    process_cmd = format("{params.java64_home}/java -cp {params.config_dir}:{extra.classpath}:{params.app_root_dir}/current/lib/* -server {java.options} -Duser.timezone={user.timezone} -Dfile.encoding={file.encoding} io.druid.cli.Main server {name()} 2>{params.app_log_dir}/druid.err 1>{params.app_log_dir}/druid.out", **params.config['configurations']['druid'])
+    conf = params.configs['druid']
+    log_dir = params.app_log_dir
+    process_cmd = format("{params.java64_home}/bin/java -cp {params.config_dir}/_common:{params.config_dir}/{node_type}:{conf[extra.classpath]} -server {conf[java.options]} -Duser.timezone={conf[user.timezone]} -Dfile.encoding={conf[file.encoding]} io.druid.cli.Main server {node_type} 2>{log_dir}/druid-{node_type}.err 1>{log_dir}/druid-{node_type}.out")
 
     Execute(process_cmd,
             user=params.app_user,
