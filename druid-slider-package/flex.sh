f23fea76e8f64ce61c3584e81416f3d6dbc03426
Jean-Baptiste Note <jb.note@criteo.com>
2015-06-16 11:43:54 +0200

    Flexibilize flex script

--- druid-slider-package/flex.sh
+++ druid-slider-package/flex.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 APPNAME=${1:-droya}
-APPNUM=${2:-12}
+APPNUM=${2:-3}
+slider flex $APPNAME --component DRUID_REALTIME $APPNUM --filesystem hdfs://root
 slider flex $APPNAME --component DRUID_HISTORICAL $APPNUM --filesystem hdfs://root
