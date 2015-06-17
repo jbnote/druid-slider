159bc513c93de0ce10be9cbc35b591cf7aef8520
Jean-Baptiste Note <jb.note@criteo.com>
2015-06-17 13:12:59 +0200

    Package GUICE dependencies from mvn before upload

--- druid-slider-package/build.sh
+++ druid-slider-package/build.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
-SOURCE=http://static.druid.io/artifacts/releases/druid-0.7.3-bin.tar.gz
+DRUID_VERSION="0.7.3-cdh5.4.2"
+#SOURCE=http://static.druid.io/artifacts/releases/druid-${DRUID_VERSION}-bin.tar.gz
+SOURCE=http://isione-hpc.criteo.grid/hadoop/druid/druid-${DRUID_VERSION}-bin.tar.gz
 TARGET=$HOME/$(basename $SOURCE)
-VERSION=$(echo $TARGET | cut -d'-' -f2)
 
 CONFIGS="appConfig.json resources.json"
 
 wget $SOURCE -O $TARGET
-mvn clean install -DskipTests -Ddruid.src=$TARGET -Ddruid.version=$VERSION
+
+# XXX rebuild the tarball by adding the dependencies
+tar zxf $TARGET
+DRUID_DIR=druid-${DRUID_VERSION}
+pushd druid-${DRUID_VERSION}
+java -cp config/_common:lib/* -Ddruid.extensions.localRepository=m2 -Ddruid.extensions.coordinates='["io.druid.extensions:druid-kafka-eight","io.druid.extensions:druid-hdfs-storage","io.druid.extensions:mysql-metadata-storage"]' -Ddruid.extensions.remoteRepositories="[ \"file://$HOME/.m2/repository\", \"https://repo1.maven.org/maven2\", \"https://metamx.artifactoryonline.com/metamx/pub-libs-releases-local\", \"https://repository.cloudera.com/artifactory/cloudera-repos\" ]" io.druid.cli.Main tools pull-deps
+popd
+NEWTAR=../$(basename $SOURCE)
+tar zcf $NEWTAR $DRUID_DIR
+rm -Rf $DRUID_DIR
+
+mvn clean install -DskipTests -Ddruid.src=$NEWTAR -Ddruid.version=$DRUID_VERSION
 unzip -o target/druid-slider-package-0.1.zip appConfig.json resources.json
